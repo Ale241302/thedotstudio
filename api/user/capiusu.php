aaa50ab50b22e41394166da61bab0a9e43e37c3c
Ale241302 <128639865+Ale241302@users.noreply.github.com>
2023-04-29 17:11:55 -0500

    subir

---
+++ api/user/capiusu.php
@@ -0,0 +1,40 @@
+<?php
+ 
+// Obtener la conexión a la base de datos
+include_once '../config/database.php';
+ 
+// Incluir las clases Capi y User
+include_once '../objects/capiusu.php';
+
+// Crear una instancia de la clase Database
+$database = new Database();
+$db = $database->getConnection();
+
+// Iniciar sesión
+session_start();
+$usuario = $_GET['usuario'];
+$rol = $_GET['rol'];
+
+// Crear una instancia de la clase Capi
+$capiusu = new Capiusu($db);
+
+// Crear una instancia de la clase User
+ 
+// Obtener los valores del formulario y asignarlos a la instancia de Capi
+$capiusu->usuario = $_POST['usuario'];
+$capiusu->capacitacion = $_POST['capacitacion'];
+
+// Start output buffering
+ob_start();
+
+// Crear el registro de capacitación
+if ($capiusu->regi()) {
+    // Redireccionar a la página "capacitaciones.php" con el parámetro "usuario" en la URL
+    header('Location: ../../capacitacionesusu.php?usuario=' . urlencode($usuario) . '&rol=' . urlencode($rol) .'');
+    // Enviar el buffer de salida al navegador
+    ob_end_flush();
+} else {
+    echo "<script>window.location.href='../../login.php';</script>";
+    exit;
+}
+?>
